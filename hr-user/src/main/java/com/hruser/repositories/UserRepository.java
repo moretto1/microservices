b1c5c79355155c262739da9e3ebf16a59b89bf27
Bruno Moretto <bmoretto404@gmail.com>
2020-12-15 19:49:51 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hruser.repositories;
+
+import com.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
